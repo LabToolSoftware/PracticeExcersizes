1dc656c40ac540d406ec068cd5cf94fd6951b4d8
Gavin Morris <gavin.stitches.morris@gmail.com>
2019-04-25 20:25:03 -0400

    This is a repository for practice excersizes in Java

---
+++ src/main/java/Item.java
@@ -0,0 +1,47 @@
+public class Item {
+    String name;
+    String description;
+    float cost;
+    int quantity;
+
+
+    public void Item(String name,String description,float cost,int quantity){
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+}
+
